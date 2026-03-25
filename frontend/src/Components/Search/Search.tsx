69f3bd21e2f781a161b71b253562786a79d9f653
Merve Duman <139700200+merveeduman@users.noreply.github.com>
2026-03-25 16:28:02 +0300

    create search

---
+++ frontend/src/Components/Search/Search.tsx
@@ -0,0 +1,16 @@
+import React, { JSX, useState } from 'react'
+
+type Props = {}
+
+const Search :React.FC<Props> = (props: Props):JSX.Element => {
+    const[search, setSearch] = useState<string>("");
+
+    const onClick = (e:any) => {
+        setSearch(e.target.value);
+        console.log(search);
+    }
+  return (<div><input value={search} onChange={(e) => onClick(e)}></input></div>)
+  
+}
+
+export default Search
