133022bead3c8358d091d1f5b68621bee29c6fc2
Merve Duman <139700200+merveeduman@users.noreply.github.com>
2026-04-02 13:00:28 +0300

    add api structure

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,14 +1,19 @@
-import React, { JSX, useState } from 'react'
+import React, { ChangeEvent, JSX, SyntheticEvent, useState } from 'react'
 
 type Props = {}
 
 const Search :React.FC<Props> = (props: Props):JSX.Element => {
     const[search, setSearch] = useState<string>("");
 
-    const onClick = (e:any) => {
+    const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
-        console.log(search);
-    }
+        console.log(e);
+    };
+
+    const onClick =  (e:SyntheticEvent) => {
+      console.log(e);
+    };
+
   return (<div><input value={search} onChange={(e) => onClick(e)}></input></div>)
   
 }
